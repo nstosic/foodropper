c90ae033d4029ee739dcfa631c75b711987ce8fe
nstosic <nemanjastosic@outlook.com>
2019-11-14 10:07:56 +0900

    MainMenu stub implementation

--- Sources/rs/co/estel/foodropper/MainMenuScene.hx
+++ Sources/rs/co/estel/foodropper/MainMenuScene.hx
@@ -3,10 +3,13 @@ package rs.co.estel.foodropper;
 import kha.Color;
 using kha.graphics2.GraphicsExtension;
 
+import rs.co.estel.foodropper.menu.MainMenu;
+
 class MainMenuScene extends Scene {
+	private var mainMenu: MainMenu;
 	
 	public function new() {
-
+		this.mainMenu = new MainMenu();
 	}
 
 	public override function onUpdate() {
@@ -15,5 +18,13 @@ class MainMenuScene extends Scene {
 
 	public override function onRender(g: kha.graphics2.Graphics) {
 		g.clear(Color.Orange);
+		var i: Int = 0;
+		for(option in mainMenu.getOptions()) {
+			g.color = Color.Green;
+			g.fillRect(40, 40 * (i + 1), 128, 32);
+			g.color = Color.White;
+			g.fillRect(96, 40 * (i + 1) + 8, 16, 16);
+			i++;
+		}
 	}
 }
