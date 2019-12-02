403a15e6b0d2d6400aeb5a43ef27111de9a5ea1e
nstosic <nemanjastosic@outlook.com>
2019-12-02 10:23:34 +0100

    Refactored main menu drawing to improve abstraction

--- Sources/rs/co/estel/foodropper/graphics/MainMenuScene.hx
+++ Sources/rs/co/estel/foodropper/graphics/MainMenuScene.hx
@@ -1,20 +1,13 @@
 package rs.co.estel.foodropper.graphics;
 import rs.co.estel.foodropper.menu.MainMenu;
-import rs.co.estel.foodropper.menu.MenuOption;
 import rs.co.estel.foodropper.introspection.Screen;
 
 import kha.Assets;
 import kha.Color;
 import kha.Font;
-import kha.graphics2.HorTextAlignment;
-import kha.graphics2.VerTextAlignment;
 using kha.graphics2.GraphicsExtension;
 
 class MainMenuScene extends Scene {
-	private static var horizontalMenuPadding: Float = 32.0;
-	private static var verticalMenuPadding: Float = 32.0;
-	private static var verticalMenuOptionSpacing: Float = 8.0;
-
 	private var mainMenu: MainMenu;
 	private var screenWidth: Float;
 	
@@ -31,13 +24,6 @@ class MainMenuScene extends Scene {
 		g.clear(Color.Black);
 		g.font = Assets.fonts.Caviar_Dreams_Bold;
 		g.fontSize = 22;
-		var i: Int = 0;
-		for(option in mainMenu.getOptions()) {
-			g.color = Color.Orange;
-			g.fillRect(MainMenuScene.horizontalMenuPadding, MainMenuScene.verticalMenuPadding + (MainMenuScene.verticalMenuOptionSpacing + MenuOption.height) * (i + 1), screenWidth - 2 * MainMenuScene.horizontalMenuPadding, MenuOption.height);
-			g.color = Color.White;
-			g.drawAlignedString(option.getCaption(), screenWidth / 2, MainMenuScene.verticalMenuPadding + (MainMenuScene.verticalMenuOptionSpacing + MenuOption.height) * (i + 1) + MenuOption.height / 2, HorTextAlignment.TextCenter, VerTextAlignment.TextMiddle);
-			i++;
-		}
+		mainMenu.draw(g, this.screenWidth);
 	}
 }
--- Sources/rs/co/estel/foodropper/menu/MainMenu.hx
+++ Sources/rs/co/estel/foodropper/menu/MainMenu.hx
@@ -1,9 +1,34 @@
 package rs.co.estel.foodropper.menu;
 
+import kha.Color;
+import kha.graphics2.HorTextAlignment;
+import kha.graphics2.VerTextAlignment;
+using kha.graphics2.GraphicsExtension;
+
 class MainMenu extends Menu {
+	private static var horizontalMenuPadding: Float = 32.0;
+	private static var verticalMenuPadding: Float = 32.0;
+	private static var verticalMenuSpacing: Float = MenuOption.height / 2;
+
+	private var textColor: Color = Color.White;
+	private var backgroundColor: Color = Color.Orange;
+
 	public function new() {
 		super();
 		this.options.add(new MenuOption('Upper option', function () { trace('Upper option tapped'); }));
 		this.options.add(new MenuOption('Lower option', function () { trace('Lower option tapped'); }));
+	}
+
+	public function draw(g: kha.graphics2.Graphics, menuWidth: Float) {
+		var x = MainMenu.horizontalMenuPadding;
+		var y = MainMenu.verticalMenuPadding;
+		var menuOptionWidth = menuWidth - MainMenu.horizontalMenuPadding * 2;
+		for(option in this.options) {
+			g.color = this.backgroundColor;
+			g.fillRect(x, y, menuOptionWidth, MenuOption.height);
+			g.color = this.textColor;
+			g.drawAlignedString(option.getCaption(), x + menuOptionWidth / 2, y + MenuOption.height / 2, HorTextAlignment.TextCenter, VerTextAlignment.TextMiddle);
+			y = y + MenuOption.height + MainMenu.verticalMenuSpacing;
+		}
 	}
 }
