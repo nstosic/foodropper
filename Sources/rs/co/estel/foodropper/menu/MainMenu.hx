69ad0ef467e9663337c956c88973a835dbb14c24
nstosic <nemanjastosic@outlook.com>
2019-12-08 13:50:07 +0100

    Refactored menu and menu option to be more centralized

--- Sources/rs/co/estel/foodropper/menu/MainMenu.hx
+++ Sources/rs/co/estel/foodropper/menu/MainMenu.hx
@@ -1,8 +1,10 @@
 package rs.co.estel.foodropper.menu;
 
+import rs.co.estel.foodropper.control.mouse.MouseController;
+import rs.co.estel.foodropper.control.mouse.MouseEvent;
+import rs.co.estel.foodropper.control.mouse.MouseEventListener;
+import kha.Assets;
 import kha.Color;
-import kha.graphics2.HorTextAlignment;
-import kha.graphics2.VerTextAlignment;
 using kha.graphics2.GraphicsExtension;
 
 class MainMenu extends Menu {
@@ -12,22 +14,56 @@ class MainMenu extends Menu {
 
 	private var textColor: Color = Color.White;
 	private var backgroundColor: Color = Color.Orange;
+	private var onMouseEvent: MouseEventListener;
 
 	public function new() {
 		super();
-		this.options.add(new MenuOption('Upper option', function () { trace('Upper option tapped'); }));
-		this.options.add(new MenuOption('Lower option', function () { trace('Lower option tapped'); }));
+		this.options.add(new MenuOption('New game', function () { this.onNewGame(); }));
+		this.options.add(new MenuOption('High score', function () { this.onHighScore(); }));
+		this.options.add(new MenuOption('Settings', function () { this.onSettings(); }));
+		this.options.add(new MenuOption('Exit', function () { this.onExit(); }));
+		this.onMouseEvent = new MouseEventListener(MouseEventListener.DOWN, function (event: MouseEvent) {
+			for (option in this.options) {
+				if (option.isWithinBounds(event.getX(), event.getY())) {
+					option.onTap();
+					return;
+				}
+			}
+		});
+		MouseController.getInstance().addListener(this.onMouseEvent);
+	}
+
+	private function onNewGame() {
+		trace('onNewGame');
+	}
+
+	private function onHighScore() {
+		trace('onHighScore');
+	}
+
+	private function onSettings() {
+		trace('onSettings');
+	}
+
+	private function onExit() {
+		trace('onExit');
+		MouseController.getInstance().removeListener(this.onMouseEvent);
 	}
 
 	public function draw(g: kha.graphics2.Graphics, menuWidth: Float) {
 		var x = MainMenu.horizontalMenuPadding;
 		var y = MainMenu.verticalMenuPadding;
+		// Draw logo
+		var logoImage = Assets.images.logo;
+		g.drawImage(logoImage, (menuWidth - logoImage.width) / 2, y);
+		// Draw options
+		y = y + logoImage.height + MainMenu.verticalMenuPadding;
 		var menuOptionWidth = menuWidth - MainMenu.horizontalMenuPadding * 2;
 		for(option in this.options) {
-			g.color = this.backgroundColor;
-			g.fillRect(x, y, menuOptionWidth, MenuOption.height);
-			g.color = this.textColor;
-			g.drawAlignedString(option.getCaption(), x + menuOptionWidth / 2, y + MenuOption.height / 2, HorTextAlignment.TextCenter, VerTextAlignment.TextMiddle);
+			option.setX(x);
+			option.setY(y);
+			option.setWidth(menuOptionWidth);
+			option.draw(g, this.textColor, this.backgroundColor);
 			y = y + MenuOption.height + MainMenu.verticalMenuSpacing;
 		}
 	}
--- Sources/rs/co/estel/foodropper/menu/MenuOption.hx
+++ Sources/rs/co/estel/foodropper/menu/MenuOption.hx
@@ -1,9 +1,19 @@
 package rs.co.estel.foodropper.menu;
 
+import kha.Color;
+import kha.graphics2.HorTextAlignment;
+import kha.graphics2.VerTextAlignment;
+using kha.graphics2.GraphicsExtension;
+
 class MenuOption {
-	public static var height: Float = 32.0;
 	private var caption: String;
 	private var onTapHandler: Void->Void;
+
+	// Area
+	public static var height: Float = 32.0;
+	private var width: Float;
+	private var x: Float;
+	private var y: Float;
 
 	public function new(caption: String, onTapHandler: Void->Void) {
 		this.caption = caption;
@@ -15,6 +25,29 @@ class MenuOption {
 	}
 
 	public function onTap() {
-		this.onTap();
+		this.onTapHandler();
+	}
+
+	public function setX(x: Float) {
+		this.x = x;
+	}
+
+	public function setY(y: Float) {
+		this.y = y;
+	}
+
+	public function setWidth(width: Float) {
+		this.width = width;
+	}
+
+	public function draw(g: kha.graphics2.Graphics, color: Color, background: Color) {
+		g.color = background;
+		g.fillRect(x, y, width, MenuOption.height);
+		g.color = color;
+		g.drawAlignedString(this.getCaption(), this.x + this.width / 2, this.y + MenuOption.height / 2, HorTextAlignment.TextCenter, VerTextAlignment.TextMiddle);
+	}
+
+	public function isWithinBounds(x: Int, y: Int): Bool {
+		return this.x < x && this.x + this.width > x && this.y < y && this.y + MenuOption.height > y;
 	}
 }
