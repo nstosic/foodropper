2f770334917f5ec9ee60c105520a3b7fe99bd11f
nstosic <nemanjastosic@outlook.com>
2019-12-15 17:30:05 +0100

    Naive implementation for starting new game

--- Sources/rs/co/estel/foodropper/Project.hx
+++ Sources/rs/co/estel/foodropper/Project.hx
@@ -1,10 +1,13 @@
 package rs.co.estel.foodropper;
 import rs.co.estel.foodropper.control.Event;
-import rs.co.estel.foodropper.graphics.Scene;
+import rs.co.estel.foodropper.graphics.BackgroundScene;
+import rs.co.estel.foodropper.graphics.InteractiveScene;
 import rs.co.estel.foodropper.graphics.MainMenuScene;
+import rs.co.estel.foodropper.graphics.Scene;
 
 import kha.Framebuffer;
 import kha.Assets;
+import kha.Color;
 using kha.graphics2.GraphicsExtension;
 
 class Project {
@@ -14,13 +17,19 @@ class Project {
 
 	public function new() {
 		this.onMainMenu = true;
-		this.mainMenuScene = new MainMenuScene();
+		this.mainMenuScene = new MainMenuScene(this.onNewGame);
 		this.scenes = new List<Scene>();
 	}
 
 	private function dispatchEvents(): List<Event> {
 		var events = new List<Event>();
 		return events;
+	}
+
+	private function onNewGame() {
+		this.scenes.add(new BackgroundScene(null));
+		this.scenes.add(new InteractiveScene());
+		this.onMainMenu = false;
 	}
 
 	public function update(): Void {
@@ -37,6 +46,7 @@ class Project {
 	public function render(framebuffer: Array<Framebuffer>): Void {
 		var graphics = framebuffer[0].g2;
 		graphics.begin();
+		graphics.clear(Color.Black);
 		if (onMainMenu) {
 			this.mainMenuScene.onRender(graphics);
 		} else {
