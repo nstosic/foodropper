2f11bea875fd87fadb2037548dcf80a2524ca536
nstosic <nemanjastosic@outlook.com>
2019-12-15 17:28:13 +0100

    Added staple background scene

--- Sources/rs/co/estel/foodropper/graphics/BackgroundScene.hx
+++ Sources/rs/co/estel/foodropper/graphics/BackgroundScene.hx
@@ -1,7 +1,9 @@
 package rs.co.estel.foodropper.graphics;
 
 import rs.co.estel.foodropper.game.World;
+import rs.co.estel.foodropper.introspection.Screen;
 
+import kha.Color;
 import kha.Image;
 
 class BackgroundScene extends Scene {
@@ -9,11 +11,15 @@ class BackgroundScene extends Scene {
 	private var offsetX: Float;
 	private var offsetY: Float;
 	private var parallaxFactor: Float;
+	private var width: Float;
+	private var height: Float;
 
 	public function new(source: Image, parallaxFactor: Float = 1.0) {
 		this.source = source;
 		this.offsetX = 0.0;
 		this.offsetY = 0.0;
+		this.width = Screen.getScreenWidth();
+		this.height = Screen.getScreenHeight();
 	}
 
 	public override function onUpdate() {
@@ -21,6 +27,7 @@ class BackgroundScene extends Scene {
 	}
 
 	public override function onRender(g: kha.graphics2.Graphics) {
-
+		g.color = Color.Green;
+		g.fillRect(0, this.height - World.GroundHeight, this.width, World.GroundHeight);
 	}
 }
