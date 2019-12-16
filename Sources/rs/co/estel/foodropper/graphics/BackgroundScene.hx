afd5de9ea3b500e95da96461795f91c30a5121d7
nstosic <nemanjastosic@outlook.com>
2019-12-16 13:01:51 +0100

    Parallax implementation with inverted image continuation

--- Sources/rs/co/estel/foodropper/graphics/BackgroundScene.hx
+++ Sources/rs/co/estel/foodropper/graphics/BackgroundScene.hx
@@ -16,12 +16,16 @@ class BackgroundScene extends Scene {
 	private var parallaxFactor: Float;
 	// Background state
 	private var offsetX: Float;
+	private var sizeX: Float;
 	private var offsetY: Float;
+	private var sizeY: Float;
 
 	public function new(source: Image, width: Float, height: Float, x: Float = 0.0, y: Float = 0.0, parallaxFactor: Float = 1.0) {
 		this.source = source;
 		this.width = width;
+		this.sizeX = width;
 		this.height = height;
+		this.sizeY = height;
 		this.x = x;
 		this.y = y;
 		this.offsetX = 0.0;
@@ -32,11 +36,18 @@ class BackgroundScene extends Scene {
 	public override function onUpdate() {
 		this.offsetX = this.offsetX + World.BackgroundXVelocity * this.parallaxFactor;
 		if (this.offsetX + this.width > source.realWidth) {
-			this.offsetX = 0.0;
+			this.sizeX = source.realWidth - this.offsetX;
+		}
+		if (this.offsetX >= source.realWidth) {
+			this.offsetX = 0;
+			this.sizeX = this.width;
 		}
 	}
 
 	public override function onRender(g: kha.graphics2.Graphics) {
-		g.drawSubImage(this.source, this.x, this.y, this.offsetX, this.offsetY, this.width, this.height);
+		g.drawSubImage(this.source, this.x, this.y, this.offsetX, this.offsetY, this.sizeX, this.sizeY);
+		if (this.sizeX != this.width) {
+			g.drawSubImage(this.source, this.sizeX, this.y, 0, 0, this.width - this.sizeX, this.sizeY);
+		}
 	}
 }
