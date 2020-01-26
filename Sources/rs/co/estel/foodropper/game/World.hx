fd0b43e87518d80ff7abf5531b72114ff6196624
nstosic <nemanjastosic@outlook.com>
2020-01-26 14:58:59 +0100

    Added jumping physics constants

--- Sources/rs/co/estel/foodropper/game/World.hx
+++ Sources/rs/co/estel/foodropper/game/World.hx
@@ -6,11 +6,19 @@ class World {
 	// Physics
 	public static var GravityEnabled: Bool = true;
 	public static var Gravity: Float = 9.81 * Unit;
+	public static var GravityDecayFactor: Float = 50.0 * Unit;
 	public static var InitialSpawnVelocity: Float = 1.0 * Unit;
+	public static var InitialJumpVelocity: Float = 6.0;
 	// Clock
+	public static var DifficultyCooldown: Float = 500 / Unit;
 	public static var InitialSpawnChance: Int = 85;
 	public static var InitialSpawnDelay: Int = 20;
 	// Graphics
 	public static var BackgroundXVelocity: Float = 1.0;
 	public static var GroundHeight: Int = 120;
+	// Metabolism
+	public static var DefaultHungerRate: Float = 0.0005;
+	public static var MaxHungerRate: Float = 0.05;
+	public static var DefaultCholesterolRate: Float = 0.0005;
+	public static var MinCholesterolRate: Float = 0.00005;
 }
